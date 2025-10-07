9bfcffde163beee363a293e7f290bff73785fec2
Solnance <larpusdx@proton.me>
2025-10-07 22:42:55 +0200

    .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": ["warn"],
+    "no-console": "off"
+  },
+};
